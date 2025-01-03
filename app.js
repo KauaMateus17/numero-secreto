4dad01bf7270d060adb708ebf32ef6d8f4821a66
KauaMateus17 <kauamateus511@gmail.com>
2025-01-02 21:11:36 -0300

    fjaskdhfl

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  }
 
  function exibirMensagemInicial() {
- exibirTextoNaTela('h1', 'Jogo do numero secreto');
+ exibirTextoNaTela('h1', 'Jogo do numero secreto!');
  exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');
  }
 
